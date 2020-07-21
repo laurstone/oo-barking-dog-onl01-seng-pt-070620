0a645ba81b32d844ada8efdd664636483b0bb760
Lauren Stone <lstone321@gmail.com>
2020-07-21 03:02:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,23 +8,12 @@ class Dog
   def name 
     @doggy_name
   end 
-  
-  def bark= (dog_do)
-    @doggy_bark = dog_do
-  end
-  
-  def bark 
-    @doggy_bark
-  end 
 end 
 
   fido = Dog.new 
     fido.name = "Fido"
   
-  fido = Dog.new
-    fido.bark = "woof!"
-    
-  puts fido.bark 
+
     
  
 
